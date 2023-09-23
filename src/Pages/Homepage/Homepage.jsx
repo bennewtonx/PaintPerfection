454aea6f2b73e6e6e05ad016a033097ecc690121
Ben Newton <ben.newton112@gmail.com>
2023-09-23 23:07:00 +0100

    homepage done, need to do media queries

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -1,41 +1,122 @@
 import React from 'react'
+import ScrollAnimationLeft from '../../Components/ScrollAnimationLeft'
+import ScrollAnimationMiddle from '../../Components/ScrollAnimationMiddle'
+import ScrollAnimation from '../../Components/ScrollAnimation'
+import CustomScrollbar from '../../Components/CustomScrollbar'
+import { motion, useScroll } from 'framer-motion';
 import './Homepage.css'
-import Header from './../../Components/Header/Header';
 
 function Homepage() {
+    const { scrollY, scrollYProgress } = useScroll(); // Track scroll position and progress
+
   return (
     <div className='homepage-container'>
-            <Header />
+        <CustomScrollbar scrollYProgress={scrollYProgress} />
         <div className='banner-container'>
-            <h1>Your dream home begins here</h1>
+            <ScrollAnimation threshold={0.5}>
+            <div className='banner-text'>
+                <p className='banner-name'>Paint Perfection</p>
+            <h1>Transform your space today</h1>
+            <button className='banner-button'>Get Started</button>
+            </div>
+            </ScrollAnimation>
+            <ScrollAnimationLeft threshold={0.5}>
+            <div className='banner-images'>
+                <div className='top-left'></div>
+                <div className='top-right'></div>
+                <div className='left'></div>
+                <div className='middle'></div>
+                <div className='right'></div>
+                <div className='bottom-left'></div>
+                <div className='bottom-right'></div>
+            </div>
+            </ScrollAnimationLeft>
         </div>
-        <div className='hp-services-container'>
-            <div className='hp-interior-text'>
-            <h1>Interior Work</h1>
-            <p>Some random, well not random, but random text about how she can do interior 
-                work to a very high degree, this will give an overview and almost sell 
-                the service so that users will click on this to see more, and HOPEFULLY 
-                book a consultation.
-            </p>
-            </div>
-            <div className='hp-interior-image'>
-            <h1>2</h1>
-            </div>
-            <div className='hp-exterior-text'>
-            <h1>Exterior Work</h1>
-            <p>Some random, well not random, but random text about how she can do exterior 
-                work to a very high degree, this will give an overview and almost sell 
-                the service so that users will click on this to see more, and HOPEFULLY 
-                book a consultation.
-            </p>
-            </div>
-            <div className='hp-exterior-image'>
-            <h1>4</h1>
-            </div>
+        <div className='overview-container'>
+            <ScrollAnimationMiddle threshold={0.5}>
+            <div className='overview-text'>
 
+
+            <h1 className='overview-heading'>Your Go-To Painter</h1>
+            <p>Welcome to the world of vibrant colors and meticulous craftsmanship, 
+                where every brushstroke counts. Our painting expert, 
+                Gypsy, is the go-to choice for homeowners and 
+                businesses in-and-around Newcastle 
+                seeking the highest quality painting service.
+            </p>
+            <div className='stats-container'>
+                <div className='jobs-done'>
+                    <h1>43</h1>
+                    <p>Jobs done</p>
+                </div>
+                <div className='jobs-done'>
+                    <h1>43</h1>
+                    <p>Homes Transformed</p>
+                </div>
+                <div className='jobs-done'>
+                    <h1>41</h1>
+                    <p>5* Reviews</p>
+                </div>
+            </div>
+            </div>
+            </ScrollAnimationMiddle>
         </div>
-        <div className='hp-reviews-container'>
-
+        <div className='services-container'>
+            <ScrollAnimation threshold={0.5}>
+            <div className='service-one'>
+                <img className='service-one-img' src='public\Screenshot_20230922-144211 (1).png'/>
+                <h3>Interior</h3>
+                <p>Our expertise will give your homes interior the glow-up it 
+                    deserves! Whether only one room needs a splash of colour or you 
+                    need a full-home makeover, we have you covered! 
+                </p>
+            </div>
+            </ScrollAnimation>
+            <ScrollAnimationLeft threshold={0.5}>
+            <div className='service-two'>
+                <img className='service-one-img' src='/Screenshot_20230922-144545.png'/>
+                <h3>Exterior</h3>
+                <p>A home is only as good as the exterior. Why settle for anything 
+                    less than your dream aesthetic. We will provide a 5* service to 
+                    make your home the talk of the street.
+                </p>
+            </div>
+            </ScrollAnimationLeft>
+        </div>
+        <div className='reviews-container'>
+            <ScrollAnimation threshold={0.5}>
+            <div className='review-one'>
+                <p>I'd been looking for a female decorator for a while then 
+                    I came across Paint Perfection on Facebook. The next day 
+                    Gypsy came out and gave me a quote! I can not fault Gypsy's 
+                    work, she's efficient, VERY precise, cleans as she works and 
+                    leaves your home immaculate. Most of all... I had some ridiculous 
+                    quotes however, Gypsy beat them all. 10/10!
+                </p>
+                <p>Diane - 16/03</p>
+            </div>
+            </ScrollAnimation>
+            <ScrollAnimationLeft threshold={0.5}>
+            <div className='review-two'>
+            <p>⭐⭐⭐⭐ Thank you for a fantastic service, can't recommend enough. 
+                We hadn't anticipated the walls would require 3 coats of paint to cover 
+                the colour underneath, Gypsy never stopped, she worked so hard all day 
+                to get it finished for us. Doors and Skirting Boards look great too! ⭐⭐⭐⭐
+                </p>
+                <p>Kirsty - 18/05</p>
+            </div>
+            </ScrollAnimationLeft>
+        </div>
+        <div className='contact-container'>
+            <ScrollAnimationMiddle threshold={0.5}>
+            <div className='contact-text'>
+                <h1>Ready to get started?</h1>
+                <p className='contact-message'>Experience the difference a professional painter can 
+                    make in your space. Contact us today to start your 
+                    painting project. </p>
+                <h3>04750375784</h3>
+            </div>
+            </ScrollAnimationMiddle>
         </div>
     </div>
   )
